ae75188f8eb1cd52dfaf7c5c783671070d6b07c6
nalamchaitanya <nalamchaitanya@gmail.com>
2016-03-09 18:40:30 +0530

    a diffusion fuction is written and diffu is renamed.

--- diffu.m
+++ diffu.m
@@ -1,26 +1,4 @@
-function a = diffu()
-    Z=[0; 4; 8; 12;];
-    Z=repmat(Z,1,4);
-    iter=0;
-    while 1
-        Y = [randperm(4);randperm(4);randperm(4);randperm(4);];
-        t = 0;
-        iter = iter+1;
-        for i = 1:4
-            for j = 1:4
-                if(size(find(Y(:,i)==j),1)~=1)
-                    t=1;
-                    break;
-                end
-            end
-            if t==1
-                break;
-            end
-        end
-        if t==0
-            break;
-        end
-    end
-    a = (Y + Z);
-    a = a([1:16]);
-    // iter
+function out = diffu(in)
+    x = csvread('diffu.csv');
+    out = in(x);
+end
