afe73b8e82d9b965a72d0e9397b7934aabf5abc0
nalamchaitanya <nalamchaitanya@gmail.com>
2016-03-09 20:56:21 +0530

    key expansion implementation is started.

---
+++ keyExp.m
@@ -0,0 +1,7 @@
+function keysch = keyExp(key,rnds)
+    // key is 64 bits so remove parity bits.
+    // then generate 'rnds' no of keys in order
+    // and store in a matrix 'keysch' which is of
+    // dimensions [rnds X 24] as 24 is our key size
+    // for each round.
+end
