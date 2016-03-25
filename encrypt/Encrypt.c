dab14594388bd2ee16727759b701be28192dd90c
nalamchaitanya <nalamchaitanya@gmail.com>
2016-03-25 15:10:43 +0530

    onehot and printbits functions are added.

--- encrypt/Encrypt.c
+++ encrypt/Encrypt.c
@@ -1,3 +1,37 @@
 #include "Encrypt.h"
 
 // Implement all the functions here.
+
+// This gives one hot at the given index.
+int onehot(int index)
+{
+    return (int)pow(2,index);
+}
+
+// This prints the int in form of bits.
+void printbits(int num)
+{
+    int i;
+    i = 31;
+    int p = (int)pow(2,31);
+    printf("<--");
+    while(i>=0)
+    {
+        printf("%d",num/p);
+        num = num<<1;
+        i--;
+    }
+    printf("-->\n");
+}
+
+// Takes a key and gives round no of keys as an array.
+int* keygen(int key)
+{
+    int *keys = (int*)malloc(sizeof(int)*ROUNDS);
+    int i = 0;
+    char l1,l2,r1,r2;
+    while(i<ROUNDS)
+    {
+        // TODO key schedule.
+    }
+}
--- encrypt/Encrypt.h
+++ encrypt/Encrypt.h
@@ -1,7 +1,13 @@
 #ifndef ENCRYPT_H
 #define ENCRYPT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
 // All #defines here.(rounds,keysize,etc.)
+#define ROUNDS 6;
 
 // Task 1: Create the substitution box as an array i.e if you give s1[64] it
 // must give the substitution of it. Fill array with appropritate values. Use
@@ -15,6 +21,16 @@ int exp[24] = {0,0,0};
 // This is the permutation array which is in the diffu.csv
 int dif[24] = {0,0,0};
 
+// This is permutation choice 1 for the key generation.
+int pc1left[2][7] = {{25,17,9,1,26,18,10},{2,27,19,11,3,12,28}};
+int pc1right[2][7] = {{31,23,15,7,30,22,14},{6,29,21,13,5,20,4}};
+
+// This is permutation choice 2 for the key generation.
+int pc2[24] = {14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2};
+
+// Shift array in each round.
+int shift[ROUNDS] = {1,2,2,1,2,1};
+
 // Takes a key and gives round no of keys as an array.
 int* keygen(int key);
 
@@ -26,5 +42,11 @@ short int subst(int sub);
 
 // This is for permutation after substitution. rearrange bits to get a short int
 short int perm(short int sub);
+
+// This gives one hot at the given index.
+int onehot(int index);
+
+// This prints the int in form of bits.
+void printbits(int num);
 
 #endif
