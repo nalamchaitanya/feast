6cf807408a6386fca8de7011679a850ac04f0e25
rohithkesa <rohithkumar162@gmail.com>
2016-03-27 11:10:55 +0530

    Code is written and has to be checked.

--- encrypt/Feast.c
+++ encrypt/Feast.c
@@ -14,6 +14,39 @@ int main(int argc,char **argv)
         exit(0);
     }
 
+
+    int i,j,flag;
+	char temp;	
+
+	global_variables();
+
+	FILE *fileptr;
+	fileptr = fopen(argv[1],"r");
+	keygen();
+	char c[4];
+	int input = 0;
+	while(1)
+	{
+		for(i=0;i<4;i++)
+		{
+			char c = fgetc(fileptr);
+			if(c == EOF)
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if(flag == 1 && i == 0)
+			break;
+		else if (flag == 1)
+		{
+			fiestel(input);
+			break;
+		}
+		else
+			fiestel(input);
+
+	}
     // Take 4 bytes at a time and convert to int and apply the encryption and
     // write into the output file. If you reach EOF then add '\0' at the end.
 }
