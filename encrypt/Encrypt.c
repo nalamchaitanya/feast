810c9f923bfd3b6b13107c673272782915f6a14d
rohithkesa <rohithkumar162@gmail.com>
2016-03-27 11:38:19 +0530

    Syntax errors are rectified.

--- encrypt/Encrypt.c
+++ encrypt/Encrypt.c
@@ -24,31 +24,10 @@ void printbits(int num)
     printf("-->\n");
 }
 
-// Takes a key and gives round no of keys as an array.
-int* keygen(int key)
-{
-    keys_arr = {1,2,3,4,5,6};
-}
-
-
-void global_variables()
-{
-    int i,j;
-    char temp;
-
-    sbox_arr        = (int *)malloc(64*sizeof(int));
-    expansion_arr   = (int *)malloc(24*sizeof(int));
-    diffusion_arr   = (int *)malloc(16*sizeof(int));
-
-    expansion_arr = {15,9,13,10,4,12,6,7,6,16,15,14,8,7,5,11,1,2,5,13,1,9,4,3};
-    diffusion_arr = {2,8,11,13,3,5,10,16,4,6,9,15,1,7,12,14};
-    sbox_arr      = {14,0,4,15,13,7,1,4,2,14,15,2,11,13,8,1,3,10,10,6,6,12,12,11,5,9,9,5,0,3,7,8,4,15,1,12,14,8,8,2,13,4,6,9,2,1,11,7,15,5,12,11,9,3,7,14,3,10,10,0,5,6,0,13};
-
-    return;
-}
 
 void fiestel(int input)
 {
+    int i;
     int left,right,temp;
     int out_left,out_right,output;
 
@@ -73,29 +52,31 @@ void fiestel(int input)
 
 int function(int input,int round_no)
 {
-    int temp;
+    int temp,temp1,i;
 
     int expanded_input = 0;
     for(i=0;i<24;i++)
     {
-        temp = input && onehot(15 - expansion_arr[i])
+        temp = input && onehot(15 - expansion_arr[i]);
         if(temp!=0)
             expanded_input += onehot(23-i); 
     }
 
-    xored_input = expanded_input ^ keys_arr[round_no];
+    int xored_input = expanded_input ^ keys_arr[round_no];
 
     int sbox_input = 0;
     for(i=0;i<4;i++)
     {
-        
-        array[i] = input && onehot(31-i);
+        temp = 0X000000FF;
+        temp1 = xored_input && temp;
+        sbox_input += sbox_arr[temp1] * onehot(8*i);
+        temp1 >> 8;
     }
 
     int diffused_input = 0;
     for(i=0;i<24;i++)
     {
-        temp = input && onehot(15 - expansion_arr[i])
+        temp = input && onehot(15 - expansion_arr[i]);
         if(temp!=0)
             diffused_input += onehot(15-i); 
     }
--- encrypt/Encrypt.h
+++ encrypt/Encrypt.h
@@ -14,23 +14,13 @@
 // Task 1: Create the substitution box as an array i.e if you give s1[64] it
 // must give the substitution of it. Fill array with appropritate values. Use
 // s1.m function in matlab to get elements in the order s1[0],s1[1]...
-int *keys_arr;
-int **sbox_arr;
-int *expansion_arr;
-int *diffusion_arr;
-
-// This is permutation choice 1 for the key generation.
-int pc1left[2][7] = {{25,17,9,1,26,18,10},{2,27,19,11,3,12,28}};
-int pc1right[2][7] = {{31,23,15,7,30,22,14},{6,29,21,13,5,20,4}};
-
-// This is permutation choice 2 for the key generation.
-int pc2[24] = {14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2};
-
-// Shift array in each round.
-int shift[ROUNDS] = {1,2,2,1,2,1};
+int keys_arr[NO_OF_ROUNDS] = {1,2,3,4,5,6};
+int expansion_arr[24] = {15,9,13,10,4,12,6,7,6,16,15,14,8,7,5,11,1,2,5,13,1,9,4,3};;
+int diffusion_arr[16] = {2,8,11,13,3,5,10,16,4,6,9,15,1,7,12,14};
+int sbox_arr[64]      = {14,0,4,15,13,7,1,4,2,14,15,2,11,13,8,1,3,10,10,6,6,12,12,11,5,9,9,5,0,3,7,8,4,15,1,12,14,8,8,2,13,4,6,9,2,1,11,7,15,5,12,11,9,3,7,14,3,10,10,0,5,6,0,13};
 
 // Takes a key and gives round no of keys as an array.
-int* keygen(int key);
+void keygen(int key);
 
 // This is expand function which expands 16 bit to 24 bit.
 int expand(short int half);
@@ -48,7 +38,6 @@ int onehot(int index);
 void printbits(int num);
 
 
-void global_variables();
 void fiestel(int input);
 int function(int input,int round_no);
 
